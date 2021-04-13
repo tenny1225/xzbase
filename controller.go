3cc6cccf42bfe1c1f34ed9ca52140705845efb58
xuzong1 <xuzong1@lenovo.com>
2021-04-13 10:57:24 +0800

    update

--- controller.go
+++ controller.go
@@ -8,15 +8,15 @@ import (
 type Controller interface {
 	SetContext(ctx *gin.Context)
 }
-type controller struct {
+type BaseController struct {
 	ctx *gin.Context
 }
 
-func (c*controller) SetContext(ctx *gin.Context) {
+func (c*BaseController) SetContext(ctx *gin.Context) {
 	c.ctx=ctx
 }
 
-func (c*controller)QueryInt64(key string,def int64) int64 {
+func (c*BaseController)QueryInt64(key string,def int64) int64 {
 	v:=c.ctx.Query(key)
 	i,e:=strconv.ParseInt(v,0,64)
 	if e!=nil{
